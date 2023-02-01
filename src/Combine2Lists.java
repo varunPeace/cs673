ade6911d4cd351de224e5b95a6e2beb1ad1b67f9
varunkumar9605 <72652472+itsmehappysoul@users.noreply.github.com>
2023-02-01 12:17:51 -0500

    Assignment 1

--- src/Combine2Lists.java
+++ src/Combine2Lists.java
@@ -1,28 +1,67 @@
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-public class Combine2Lists {
-    public static void main(String[] args){
-        ArrayList<String> l1 = new ArrayList<String>();
+class Combine2Lists {
+
+    public static void main(String[] args)
+    {
+        List<String> l1 = new ArrayList<String>();
         l1.add("a");
         l1.add("b");
         l1.add("c");
 
-        ArrayList<String> l2= new ArrayList<String>();
+        List<String> l2 = new ArrayList<String>();
         l2.add("1");
         l2.add("2");
         l2.add("3");
 
-       ArrayList<String> cl = new ArrayList<String>();
+        System.out.print("List1 contents: ");
 
-        for (int k = 0; k < 10 ; k++){
-            if(k%2!=0)
-                cl.add(l1.get(k));
-            else
-                cl.add(l2.get(k));
+        Iterator iterator = l1.iterator();
+
+        while (iterator.hasNext())
+            System.out.print(iterator.next() + " ");
+
+        System.out.println();
+
+        System.out.print("List2 contents: ");
+
+        iterator = l2.iterator();
+
+        while (iterator.hasNext())
+            System.out.print(iterator.next() + " ");
+
+        System.out.println();
+
+        int i = 0;
+        int j = 0;
+
+        List<String> merged_list = new ArrayList<String>();
+
+        while (i < l1.size() && j < l2.size()) {
+            merged_list.add(l1.get(i));
+            merged_list.add(l2.get(j));
+            i++;
+            j++;
         }
 
-        System.out.println("Combined List:"+cl);
+        while (i < l1.size()) {
+            merged_list.add(l1.get(i));
+            i++;
+        }
 
+        while (j < l2.size()) {
+            merged_list.add(l2.get(j));
+            j++;
+        }
+
+        System.out.print("Merged List contents: ");
+
+        iterator = merged_list.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
     }
 }
-
